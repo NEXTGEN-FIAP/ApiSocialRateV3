7ef3ab426c0afac77bc4803b4f9439b0aaba3e53
ricardo-temple <ricardovtemple@gmail.com>
2024-05-07 18:33:01 -0300

    Adiciona metodo cadastra Campanha vinculado a uma empresa

--- src/main/java/com/example/ApiSocialRateV3/service/EmpresaService.java
+++ src/main/java/com/example/ApiSocialRateV3/service/EmpresaService.java
@@ -1,25 +1,50 @@
 package com.example.ApiSocialRateV3.service;
 
+import com.example.ApiSocialRateV3.controller.dto.CampanhaDTO;
 import com.example.ApiSocialRateV3.controller.dto.EmpresaDTO;
+import com.example.ApiSocialRateV3.model.Campanha;
 import com.example.ApiSocialRateV3.model.Empresa;
+import com.example.ApiSocialRateV3.repository.CampanhaRepository;
 import com.example.ApiSocialRateV3.repository.EmpresaRepository;
 import lombok.NonNull;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.Optional;
 
 @Service
 public class EmpresaService {
     @Autowired
     private EmpresaRepository empresaRepository;
+    @Autowired
+    private CampanhaRepository campanhaRepository;
 
     @Transactional(propagation = Propagation.REQUIRED)
     public Empresa cadastrarEmpresa(@NonNull EmpresaDTO empresaDTO){
         Empresa empresa = new Empresa(empresaDTO);
         return empresaRepository.save(empresa);
+    }
+
+    public Campanha cadastrarCampanha(@NonNull CampanhaDTO campanhaDTO, Long id) {
+        Optional<Empresa> optionalEmpresa = empresaRepository.findById(id);
+
+        if (optionalEmpresa.isPresent()) {
+            Empresa empresa = optionalEmpresa.get();
+
+            Campanha novaCampanha = new Campanha(campanhaDTO);
+            campanhaRepository.save(novaCampanha);
+
+            empresa.adicionarCampanha(novaCampanha);
+
+            empresaRepository.save(empresa);
+
+            return novaCampanha;
+        } else {
+            throw new IllegalArgumentException("Nao foi possivel localiza a empresa com ID: " + id);
+        }
     }
 
     public void deletarEmpresaPorId(Long id){
